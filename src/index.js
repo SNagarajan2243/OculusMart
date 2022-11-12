4f19518d74a706493475df29f09313311feeb127
Shivay2243 <85389135+SNagarajan2243@users.noreply.github.com>
2022-11-12 22:40:10 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const reveal = () => {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+};
+
+window.addEventListener("scroll", reveal);
+
+let section = document.getElementById("Number-count")
+let start = false
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+
+window.onscroll = function(){
+  if(window.scrollY >= section.offsetTop){
+    if(!start){
+      valueDisplays.forEach((valueDisplay)=>{ startCount(valueDisplay)})
+    }
+    start = true
+  }
+}
+
+const startCount = (valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-limit"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+}
